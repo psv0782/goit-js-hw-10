2357b000df604f7f82054c7ddcba1d31bf821b9e
psv0782 <78725576+psv0782@users.noreply.github.com>
2025-04-20 19:19:36 +0000

    Deploying to gh-pages from @ psv0782/goit-js-hw-10@965306dd41b1c4576801b3c67e9533bd8454ff6c 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i as s}from"./assets/vendor-A92OCY9B.js";const o=document.querySelector(".form");o.addEventListener("submit",n);function n(i){i.preventDefault();const t=Number(o.delay.value),r=o.state.value;new Promise((e,m)=>{setTimeout(()=>{r==="fulfilled"?e(t):m(t)},t)}).then(e=>{s.show({message:`Fulfilled promise in ${e}ms`,color:"green",position:"topCenter",timeout:`${e}`})}).catch(e=>{s.show({message:`Rejected promise in ${e}ms`,color:"red",position:"topCenter",timeout:`${e}`})})}
+//# sourceMappingURL=2-snackbar.js.map
